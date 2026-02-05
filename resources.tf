8ab230c98994c9eb770ca7a2030d1562426a4d24
stas <stas@Terraform.local>
2026-02-05 15:10:37 +0000

    Add subnets

--- resources.tf
+++ resources.tf
@@ -1,8 +1,26 @@
 resource "aws_vpc" "exam_vpc" {
-  cidr_block = var.cidr_block
+  cidr_block = var.vpc_cidr_block
   tags = {
     "Name" = "Exam VPC"
     "Created"="terraform"
   }
 }
 
+resource "aws_subnet" "exam_private_subnet" {
+  vpc_id = aws_vpc.exam_vpc.id
+  cidr_block = var.private_subnet_cidr
+  tags = {
+    "Name"="Private Subnet"
+    "Created"="terraform"
+  }
+}
+
+resource "aws_subnet" "exam_public_subnet" {
+  vpc_id = aws_vpc.exam_vpc.id
+  cidr_block = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    "Name"="Public Subnet"
+    "Created"="terraform"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -13,8 +13,15 @@ variable "region" {
 }
 
 # networking variables
-variable "cidr_block" {
+variable "vpc_cidr_block" {
   type    = string
   default = "10.0.0.0/16"
 }
-
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+variable "private_subnet_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
