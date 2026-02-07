1499145a05634f57511703398ea2121039ad8993
Stanislav Mestechkin <s.mestechkin@gmail.com>
2026-02-07 15:39:22 +0000

    Add security group

--- instances.tf
+++ instances.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "exam_instance" {
   ami                         = "ami-0801976e993cd4ddf"
   instance_type               = "t2-micro"
+  security_groups = [ aws_security_group.exam_sg ]
   subnet_id                   = aws_subnet.public_subnet.id
   associate_public_ip_address = true
   tags = {
