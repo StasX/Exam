9a855904ff3389b99d6e5bc377fbff6f1374091f
Stanislav Mestechkin <s.mestechkin@gmail.com>
2026-02-07 15:22:22 +0000

    Add Instance

---
+++ instances.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "exam_instance" {
+  ami                         = "ami-0801976e993cd4ddf"
+  instance_type               = "t2-micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  tags = {
+    "Name"    = "Exam Machine"
+    "Created" = "Terraform"
+  }
+}
